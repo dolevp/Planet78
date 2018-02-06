9fb0129ebabb6cfd3007bbcb5ab20e4ed714cc11
Dolev Pearl <19dolevp@gmail.com>
2018-02-06 11:20:32 +0000

    No Comments

---
+++ Assets/Scripts/Fading.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fading : MonoBehaviour {
+
+	public Texture2D fadeOutTexture;
+	public float fadeSpeed = 0.8f;
+	private int drawDepth = -1000;
+	private float alpha = 1.0f;
+	private int fadeDir = -1;
+	// Use this for initialization
+	void OnGUI () {
+
+		alpha += fadeDir * fadeSpeed * Time.deltaTime;
+		alpha = Mathf.Clamp01 (alpha);
+
+		GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+		GUI.depth = drawDepth;
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), fadeOutTexture);
+	}
+
+	public float BeginFade (int dir){
+
+		fadeDir = dir;
+		return(fadeSpeed);
+
+	}
+
+	void OnLevelWasLoaded(){
+
+		BeginFade (-1);
+
+	}
+	
+	// Update is called once per frame
+
+}
