def8f16d2af6549059152bb4ac72cea6279bc60a
Dolev Pearl <19dolevp@gmail.com>
2018-02-06 15:48:53 +0000

    working on shop >

--- Assets/Scripts/ShopController.cs
+++ Assets/Scripts/ShopController.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SetMoney : MonoBehaviour {
+public class ShopController : MonoBehaviour {
 
-	Text moneyText;
 	// Use this for initialization
 	void Start () {
-
-		moneyText = GetComponent<Text> ();
-		moneyText.text = "" + PlayerPrefs.GetInt ("Cash");
-
+		
 	}
 	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 
 }
