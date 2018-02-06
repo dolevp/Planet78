39245880f2a30f6d56609ac7028f53b491760332
Dolev Pearl <19dolevp@gmail.com>
2018-02-06 14:04:37 +0000

    amazing. todo: add ads, shop and leaderboard

---
+++ Assets/Scripts/heartbeat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heartbeat : MonoBehaviour {
+
+	public Earth earth;
+	public Animator anim;
+	// Use this for initialization
+	void Start(){
+
+		anim = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		anim.speed += 0.006f * Time.deltaTime;
+	}
+}
